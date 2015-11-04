d6d87b2f6a915db8df9217f9a486d3865a2cffae
connyliu <21165491@qq.com>
2015-11-04 19:41:54 +0800

    源码上传

---
+++ EUEx3DTouch/EUEx3DTouch/EUEx3DTouch.h
@@ -0,0 +1,13 @@
+//
+//  EUEx3DTouch.h
+//  EUEx3DTouch
+//
+//  Created by CeriNo on 15/11/4.
+//  Copyright © 2015年 AppCan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "EUExBase.h"
+@interface EUEx3DTouch : EUExBase
+
+@end
---
+++ EUEx3DTouch/EUEx3DTouch/EUEx3DTouch.m
@@ -0,0 +1,100 @@
+//
+//  EUEx3DTouch.m
+//  EUEx3DTouch
+//
+//  Created by CeriNo on 15/11/4.
+//  Copyright © 2015年 AppCan. All rights reserved.
+//
+
+#import "EUEx3DTouch.h"
+#import "JSON.h"
+
+@interface EUEx3DTouch()
+
+@end
+
+@implementation EUEx3DTouch
+
+
+
+
+
+
+-(void)setDynamicShortcutItems:(NSMutableArray *)inArguments{
+    if([inArguments count] < 1){
+        return;
+    }
+    id info = [inArguments[0] JSONValue];
+    if(!info || ![info isKindOfClass:[NSArray class]]){
+        return;
+    }
+    NSMutableArray<UIApplicationShortcutItem *> *shortcuts=[NSMutableArray array];
+    for(int i=0;i<[info count];i++){
+        UIApplicationShortcutItem *item=[self parseShortcutItem:info[i]];
+        if(item){
+            [shortcuts addObject:item];
+        }
+    }
+    [[UIApplication sharedApplication] setShortcutItems:shortcuts];
+    
+}
+-(UIApplicationShortcutItem *)parseShortcutItem:(NSDictionary *)shortcutInfo{
+    if(!shortcutInfo || ![shortcutInfo isKindOfClass:[NSDictionary class]]){
+        return nil;
+    }
+    if(![shortcutInfo objectForKey:@"type"] || ![shortcutInfo objectForKey:@"title"]){
+        return nil;
+    }
+    UIMutableApplicationShortcutItem *item =[[UIMutableApplicationShortcutItem alloc]initWithType:[shortcutInfo objectForKey:@"type"]
+                                                                                   localizedTitle:[shortcutInfo objectForKey:@"title"]];
+    UIApplicationShortcutIcon *icon=nil;
+    NSArray *iconArray=@[@"UIApplicationShortcutIconTypeCompose",
+                         @"UIApplicationShortcutIconTypePlay",
+                         @"UIApplicationShortcutIconTypePause",
+                         @"UIApplicationShortcutIconTypeAdd",
+                         @"UIApplicationShortcutIconTypeLocation",
+                         @"UIApplicationShortcutIconTypeSearch",
+                         @"UIApplicationShortcutIconTypeShare",
+                         @"UIApplicationShortcutIconTypeProhibit",
+                         @"UIApplicationShortcutIconTypeContact",
+                         @"UIApplicationShortcutIconTypeHome",
+                         @"UIApplicationShortcutIconTypeMarkLocation",
+                         @"UIApplicationShortcutIconTypeFavorite",
+                         @"UIApplicationShortcutIconTypeLove",
+                         @"UIApplicationShortcutIconTypeCloud",
+                         @"UIApplicationShortcutIconTypeInvitation",
+                         @"UIApplicationShortcutIconTypeConfirmation",
+                         @"UIApplicationShortcutIconTypeMail",
+                         @"UIApplicationShortcutIconTypeMessage",
+                         @"UIApplicationShortcutIconTypeDate",
+                         @"UIApplicationShortcutIconTypeTime",
+                         @"UIApplicationShortcutIconTypeCapturePhoto",
+                         @"UIApplicationShortcutIconTypeCaptureVideo",
+                         @"UIApplicationShortcutIconTypeTask",
+                         @"UIApplicationShortcutIconTypeTaskCompleted",
+                         @"UIApplicationShortcutIconTypeAlarm",
+                         @"UIApplicationShortcutIconTypeBookmark",
+                         @"UIApplicationShortcutIconTypeShuffle",
+                         @"UIApplicationShortcutIconTypeAudio",
+                         @"UIApplicationShortcutIconTypeUpdate"];
+
+    
+    if([shortcutInfo objectForKey:@"iconType"] && [iconArray containsObject:[shortcutInfo objectForKey:@"iconType"]]){
+        icon=[UIApplicationShortcutIcon iconWithType:[iconArray indexOfObject:[shortcutInfo objectForKey:@"iconType"]]];
+    }
+    if([shortcutInfo objectForKey:@"iconFile"] && [shortcutInfo[@"iconFile"] isKindOfClass:[NSString class]]){
+        icon=[UIApplicationShortcutIcon iconWithTemplateImageName:shortcutInfo[@"iconFile"]];
+    }
+    if(icon){
+        [item setIcon:icon];
+    }
+    if([shortcutInfo objectForKey:@"info"] && [shortcutInfo[@"info"] isKindOfClass:[NSDictionary class]]){
+        [item setUserInfo:shortcutInfo[@"info"]];
+    }
+    if([shortcutInfo objectForKey:@"subtitle"] && [shortcutInfo[@"subtitle"] isKindOfClass:[NSString class]]){
+        [item setLocalizedSubtitle:shortcutInfo[@"subtitle"]];
+    }
+    return item;
+}
+
+@end
